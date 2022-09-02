6785625d05b20f6b9620ad17fab97905fb8fbc68
AhmadAwali1995 <112668126+AhmadAwali1995@users.noreply.github.com>
2022-09-02 03:49:07 +0300

    Add files via upload

---
+++ TestApiJWT/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using TestApiJWT.Models;
+
+namespace TestApiJWT.Services
+{
+    public interface IAuthService
+    {
+        public Task<AuthModel> RegisterAsync(RegisterModel model);
+        public Task<AuthModel> GetTokenAsync(TokenRequestModel model);
+        public Task<string> AddRoleAsync(RoleModel model);
+    }
+}
